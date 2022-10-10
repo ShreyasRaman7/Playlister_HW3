59df07a9bd8228132f1ff4171005a47df901fb55
ShreyasRaman7 <71574041+ShreyasRaman7@users.noreply.github.com>
2022-10-10 12:08:37 -0400

    first commit-shreyas

--- server/db/index.js
+++ server/db/index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
     
     @author McKilla Gorilla
 */
+
 mongoose
     .connect('mongodb://127.0.0.1:27017/playlists', { useNewUrlParser: true })
     .catch(e => {
